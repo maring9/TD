795280409baaeec79b265be961b072f8866cc54d
Marin <marin.getejanc.upt@gmail.com>
2022-10-25 17:53:59 +0300

    Final edit

--- parser.c
+++ parser.c
@@ -5,85 +5,105 @@
 #define CR 0x0D
 #define LF 0x0A
 
+// Save data
 AT_COMMAND_DATA command_data;
+
 int current_state = 0;
-int rows = 0;
-int line = -1;
-int column = 0;
 int num_transmissions = 0;
+int aux_row = 0;
+int line_count = -1;
+int column_index = 0;
+
 int char_is(uint8_t expected_char, uint8_t current_char)
 {
     return current_char == expected_char;
 }
 
 // Wrappers for readability
-// Not used
+// Not used anymore
 int char_is_CR(uint8_t current_char)
 {
     return current_char == 0x0D;
 }
 
+// Wrappers for readability
+// Not used anymore
 int char_is_LF(uint8_t current_char)
 {
     return current_char == 0x0A;
 }
 
+// Wrappers for readability
 int char_is_valid_ascii(uint8_t current_char)
 {
     return current_char >= 32 && current_char <= 126;
 }
 
+// To check if there is space to save data
 int space_to_save_lines(int num_lines)
 {
     return num_lines < AT_COMMAND_MAX_LINES;
 }
 
+// To check for max line size
 int space_to_save_chars(int num_chars)
 {
     return num_chars < AT_COMMAND_MAX_LINE_SIZE;
 }
 
-/*
-void initialize_fsm()
-{
-    num_transmissions = 0;
-    line = -1;
-    column = 0;
-    rows = 0;
-}
-*/
-
-void show() {
+// Function to print FSM data
+void print_data() {
     printf("Transmission %d:\n", num_transmissions);
-    for (int i = 0; i <= rows; i++) {
+    for (int i = 0; i <= aux_row; i++) {
         printf("%s\n", command_data.data[i]);
     }
 }
 
-void restart() {
-    for (int i = 0; i <= rows; i++) {
+// 
+void reset_data()
+{
+    for (int i = aux_row; i >= 0; --i) {
         memset(command_data.data[i], 0, sizeof command_data.data[i]);
     }
+}
+
+void reset_states()
+{
     current_state = 0;
-    rows = 0;
-    line = -1;
-    column = 0;
+    aux_row = 0;
+    line_count = -1;
+    column_index = 0;
+}
+
+void reset_fsm() {
+    reset_data();
+    reset_states();
+}
+
+void finished_line()
+{
+    line_count++;
+    column_index = 0;
+}
+
+void save_char(uint8_t current_char)
+{
+    command_data.data[line_count][column_index] = current_char;
+    column_index++;
 }
 
 STATE_MACHINE_RETURN_VALUE at_command_parse(uint8_t current_char)
 {
 
-    if (space_to_save_lines(line) && space_to_save_chars(column)) {
+    if (space_to_save_lines(line_count) && space_to_save_chars(column_index)) {
             if (char_is(LF, current_char)) {
-                line++;
-                column = 0;
+                finished_line();
             } else if (!char_is(CR, current_char) && !char_is('+', current_char)) {
-                command_data.data[line][column] = current_char;
-                column++;
+                save_char(current_char);
             }
         }
-    if (line > rows && line < AT_COMMAND_MAX_LINES) {
-        rows = line;
+    if (line_count > aux_row && line_count < AT_COMMAND_MAX_LINES) {
+        aux_row = line_count;
     }
     switch (current_state)
     {
@@ -175,8 +195,8 @@ STATE_MACHINE_RETURN_VALUE at_command_parse(uint8_t current_char)
         if (char_is(LF, current_char))
         {
             num_transmissions++;
-            show();
-            restart();
+            print_data();
+            reset_fsm();
             // printf("Current state: 5, got char %d\n", current_char);
             // printf("No data - parsed CRLF OK CRLF\n");
             return STATE_MACHINE_READY_OK;
